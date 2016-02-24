e8e2f6094135f99865b564b2d0c8aa04919927f5
Dom Leone <ohheydom@gmail.com>
2016-02-24 12:32:14 -0500

    Fixed formatting. Added more to readme.

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-### Titanic - Machine Learning From Disaster
-This is my attempt at creating a classifier from a dataset of Titanic passengers. The dataset is from a [Kaggle](http://www.kaggle.com) competition. I've used python and the sklearn library. 
+# Titanic - Machine Learning From Disaster
+This is my attempt at creating a classifier from a dataset of Titanic passengers. The dataset is from a [Kaggle](http://www.kaggle.com) competition. It uses a Random Forest Classifier from the [sklearn library](http://scikit-learn.org/stable/).
 
-#### Files
+## Usage
+
+In the terminal, simply type
+
+```
+python titanic.py
+```
+
+## Files
 * train.csv - training data with labels
 * test.csv - testing data without labels
-* titanic_classifier.py - main Python file, to run, type ```python titanic.py```
+* titanic_classifier.py - main Python file to load, preprocess, and predict data
 * rfpredictions.csv - Predictions on the test data
--- titanic.py
+++ titanic.py
@@ -12,25 +12,25 @@ def clean_data(data):
     # Fill in missing fare values with means of each Pclass
     data.Fare = data.Fare.map(lambda x: np.nan if x == 0 else x)
     class_means = data.pivot_table('Fare', index='Pclass', aggfunc='mean')
-    data.Fare = data[['Fare', 'Pclass']].apply(lambda x: class_means[x['Pclass']] if pd.isnull(x['Fare']) else x['Fare'], axis = 1)
+    data.Fare = data[['Fare', 'Pclass']].apply(lambda x: class_means[x['Pclass']] if pd.isnull(x['Fare']) else x['Fare'], axis=1)
 
     # Remove Nan from Cabin information
     data.Cabin = data.Cabin.fillna('Unknown')
 
     # One-Hot Encoding of Embarked feature
     embarked = pd.get_dummies(data[['Embarked']])
-    data = pd.concat([data.drop('Embarked', 1), embarked], axis = 1)
+    data = pd.concat([data.drop('Embarked', 1), embarked], axis=1)
 
     # One-Hot Encoding of Sex feature
     sex = pd.get_dummies(data[['Sex']])
-    data = pd.concat([data.drop('Sex', 1), sex], axis = 1)
+    data = pd.concat([data.drop('Sex', 1), sex], axis=1)
 
     return data
 
 # Load data
 training_data = pd.read_csv('train.csv')
 y = training_data.Survived
-training_data = training_data.drop('Survived', 1)
+training_data.drop('Survived', axis=1, inplace=True)
 testing_data = pd.read_csv('test.csv')
 
 # Preprocess
@@ -39,27 +39,26 @@ testing_data = clean_data(testing_data)
 
 # Feature Selection
 features = ['Pclass', 'Age', 'Fare', 'Sex_male', 'Sex_female']
-selected_data = training_data[features]
+selected_training_data = training_data[features]
 selected_testing_data = testing_data[features]
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(selected_data, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(selected_training_data, y, test_size=0.2, random_state=0)
 
 # Fit training data
 params = {'max_features':(1, 2, 3, 4, 5)}
 rf = RandomForestClassifier()
-sss = StratifiedShuffleSplit(y_train, 10, test_size = 0.1, random_state = 0)
-gs = GridSearchCV(estimator = rf, param_grid = params, cv = sss)
+sss = StratifiedShuffleSplit(y_train, 10, test_size=0.1, random_state=0)
+gs = GridSearchCV(estimator=rf, param_grid=params, cv=sss)
 gs.fit(X_train, y_train)
 clf = gs.best_estimator_
 
 # Predict on test split
 print("Accuracy score: %f" % accuracy_score(clf.predict(X_test), y_test))
 
-
 # Predict unlabeled test data
 pred = clf.predict(selected_testing_data)
 
 # Save results to file
 n = pd.DataFrame(data={'PassengerId': testing_data['PassengerId'], 'Survived': pred})
-n.to_csv('rfpredictions.csv', index = False)
+n.to_csv('rfpredictions.csv', index=False)
